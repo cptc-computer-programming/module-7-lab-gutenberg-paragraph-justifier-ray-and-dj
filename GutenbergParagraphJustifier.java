9575ae87233fae783976d7c167749df2c8eaae85
Ray Stallard <rinmynth@gmail.com>
2026-08-13 10:18:33 -0700

    started program

--- GutenbergParagraphJustifier.java
+++ GutenbergParagraphJustifier.java
@@ -1,20 +1,21 @@
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Scanner;
+import java.io.File;
 
 public class GutenbergParagraphJustifier {
 public static final int TAB_SIZE = 4;
 
     public static void main(String[] args) throws FileNotFoundException {
         // create scanners
-
+        Scanner input = new Scanner(new File("data\\input\\example.txt"));
         // create output stream
 
 
         // process
 
         // close scanners
-
+        input.close();
     }
 
     // Reads the input text and writes a "justified" version:
@@ -22,6 +23,10 @@ public static final int TAB_SIZE = 4;
     // - collapses multiple blank lines into ONE blank line
     // - indents every nonblank line by TAB_SIZE spaces
     public static void justifyParagraphs(Scanner input, PrintStream out) {
+        input.nextLine();
+        while (input.hasNextLine()) {
+            
+        }
     }
 
     // Prompts until a readable file name is provided, then returns a Scanner on it.
